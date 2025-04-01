92a8a48539948bd26520d6fba05498b89ff4551c
Patrick Drechsler <patrick.drechsler@mathema.de>
2025-04-01 11:36:50 +0200

    🧪 add more failing tests

--- TestProject5/UnitTest1.cs
+++ TestProject5/UnitTest1.cs
@@ -10,9 +10,28 @@ public class UnitTest1
         var homer = new Person(1, "Homer changed", "Simpson");
         return Verify(homer);
     }
-
+        
     [Fact]
     public void Test2_doesnt_open_vscode_diff_viewer_when_executed_from_vscode()
+    {
+        var info = new ProcessStartInfo(@"%PROGRAMFILES%\Microsoft VS Code\Code.exe");
+        info.CreateNoWindow = true;
+        info.Arguments = @"--diff ""C:\tmp\TestProject5\TestProject5\UnitTest1.Test1.received.txt"" ""C:\tmp\TestProject5\TestProject5\UnitTest1.Test1.verified.txt""";
+   
+        Process.Start(info);
+    }
+    [Fact]
+    public void Test3_doesnt_open_vscode_diff_viewer_when_executed_from_vscode()
+    {
+        var info = new ProcessStartInfo(@"C:\Program Files\Microsoft VS Code\Code.exe");
+        info.CreateNoWindow = true;
+        info.Arguments = @"--diff ""C:\tmp\TestProject5\TestProject5\UnitTest1.Test1.received.txt"" ""C:\tmp\TestProject5\TestProject5\UnitTest1.Test1.verified.txt""";
+   
+        Process.Start(info);
+    }
+
+    [Fact]
+    public void Test4_doesnt_open_vscode_diff_viewer_when_executed_from_vscode()
     {
         var info = new ProcessStartInfo(@"%PROGRAMFILES%\Microsoft VS Code\bin\code.cmd");
         info.CreateNoWindow = true;
@@ -22,7 +41,7 @@ public class UnitTest1
     }
 
     [Fact]
-    public void Test3_opens_vscode_diff_viewer_when_executed_from_vscode()
+    public void Test5_opens_vscode_diff_viewer_when_executed_from_vscode()
     {
         // This works as expected:
         var info = new ProcessStartInfo(@"C:\Program Files\Microsoft VS Code\bin\code.cmd");
